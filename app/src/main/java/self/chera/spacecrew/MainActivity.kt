ffe301ae629a1f30f3fd21cf21447518025eff0d
keychera <kyogatama@gmail.com>
2024-06-05 12:33:51 +0900

    ask for relevant bluetooth permission

--- app/src/main/java/self/chera/spacecrew/MainActivity.kt
+++ app/src/main/java/self/chera/spacecrew/MainActivity.kt
@@ -1,11 +1,14 @@
+@file:OptIn(ExperimentalPermissionsApi::class)
+
 package self.chera.spacecrew
 
+import android.Manifest
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.animation.AnimatedContentTransitionScope
-import androidx.compose.animation.core.tween
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -22,6 +25,9 @@ import androidx.compose.ui.unit.dp
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.google.accompanist.permissions.ExperimentalPermissionsApi
+import com.google.accompanist.permissions.MultiplePermissionsState
+import com.google.accompanist.permissions.rememberMultiplePermissionsState
 import self.chera.spacecrew.ui.theme.Spacecrew2Theme
 
 class MainActivity : ComponentActivity() {
@@ -39,13 +45,30 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Root(
-    modifier: Modifier = Modifier
-) {
+fun Root(modifier: Modifier = Modifier) {
+    val bluetoothPermissionState = rememberMultiplePermissionsState(
+        listOf(
+            Manifest.permission.BLUETOOTH,
+            Manifest.permission.BLUETOOTH_SCAN,
+            Manifest.permission.BLUETOOTH_CONNECT,
+            Manifest.permission.BLUETOOTH_ADMIN,
+            Manifest.permission.ACCESS_COARSE_LOCATION,
+            Manifest.permission.ACCESS_FINE_LOCATION,
+        ),
+    )
+
+    if (bluetoothPermissionState.allPermissionsGranted) {
+        Navigation(modifier)
+    } else {
+        AskForBluetooth(permissionStates = bluetoothPermissionState, modifier)
+    }
+}
+@Composable
+fun Navigation(modifier: Modifier = Modifier) {
     val navController = rememberNavController()
     NavHost(navController, startDestination = "main") {
         composable("main") {
-            Main(
+            ConnectedDevices(
                 onClickScanForDevice = { navController.navigate("scan") },
                 modifier = modifier
             )
@@ -61,7 +84,7 @@ fun Root(
 }
 
 @Composable
-fun Main(
+fun ConnectedDevices(
     onClickScanForDevice: () -> Unit,
     modifier: Modifier = Modifier
 ) {
@@ -85,9 +108,27 @@ fun Main(
 }
 
 @Composable
+fun AskForBluetooth(
+    permissionStates: MultiplePermissionsState,
+    modifier: Modifier
+) {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
+        modifier = modifier.fillMaxSize()
+    ) {
+        Text("Bluetooth is needed")
+        Button(onClick = { permissionStates.launchMultiplePermissionRequest() }) {
+            Text("Grant permission")
+        }
+    }
+}
+
+@Composable
 fun DeviceScan(
     modifier: Modifier = Modifier
 ) {
+
     Column(
         modifier = modifier.padding(24.dp)
     ) {
